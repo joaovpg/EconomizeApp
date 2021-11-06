2c47d3c5eb6d3a4e06756af3925a9b3ee375bb39
JuaumMitty <jjoaovictorpg@gmail.com>
2021-11-06 15:17:36 -0300

    Corrigindo falhas de segurança

--- EconomizeBack/server/server/settings.py
+++ EconomizeBack/server/server/settings.py
@@ -163,5 +163,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'testeconomize@gmail.com'
-EMAIL_HOST_PASSWORD = 'Neon@3160'
+EMAIL_HOST_USER = '*****'
+EMAIL_HOST_PASSWORD = '******'
