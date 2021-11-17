3b05231434b6fe267d21e3b972f344a5a92b53e5
ThallysaaS <thallysspeed4@gmail.com>
2021-11-16 22:05:18 -0300

    Criandos as views das transações contas

--- EconomizeBack/server/transations/views.py
+++ EconomizeBack/server/transations/views.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import TransationsSerializer, ContasSerializer, CategoriasSerializer, SubcategoriaSerializer
-from .models import Categorias, Contas, Subcategoria, Transations, User
+from .serializers import TransationsSerializer, ContasSerializer, CategoriasSerializer, ContasDetailSerializer
+from .models import Categorias, Contas, Transations, User
 from rest_framework import permissions
 from .permissions import IsOwner
 # Create your views here.
@@ -22,12 +22,26 @@ class ListContasView(generics.ListCreateAPIView):
 
 class ContasDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Contas.objects.all()
-    serializer_class = ContasSerializer
+    serializer_class = ContasDetailSerializer
+    lookup_field = "id"
+    permission_classes = (permissions.IsAuthenticated,)
 
+    def perform_create(self, serializer):
+        return serializer.save(idUsuario=self.request.user)
+
+ #   Filtra as informações pelo id do usuário
+    def get_queryset(self):
+        return self.queryset.filter(idUsuario=self.request.user)
 
 class ListTransationsView(generics.ListCreateAPIView):
     queryset = Transations.objects.all()
     serializer_class = TransationsSerializer
+
+    def perform_create(self, serializer):
+         return serializer.save(idUsuario=self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(idUsuario=self.request.user)
 
 
 class TransationsDetailView(generics.RetrieveUpdateDestroyAPIView):
@@ -38,18 +52,15 @@ class TransationsDetailView(generics.RetrieveUpdateDestroyAPIView):
 class ListCategoriasView(generics.ListCreateAPIView):
     queryset = Categorias.objects.all()
     serializer_class = CategoriasSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        return serializer.save(idUsuario=self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(idUsuario=self.request.user)
 
 
 class CategoriasDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Categorias.objects.all()
     serializer_class = CategoriasSerializer
-
-
-class ListSubcategoriaView(generics.ListCreateAPIView):
-    queryset = Subcategoria.objects.all()
-    serializer_class = SubcategoriaSerializer
-
-
-class SubcategoriaDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Subcategoria.objects.all()
-    serializer_class = SubcategoriaSerializer
